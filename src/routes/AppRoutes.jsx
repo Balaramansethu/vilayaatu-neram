b12ba95959248ddb0ea1690e62cfbac4cd0ad32e
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2025-01-03 20:17:49 +0530

    UPDATE - Update route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -13,6 +13,7 @@ const AppRoutes = () => {
         <Route path='/signup' element={<SignupPage/>}/>
         <Route path='/request-otp' element={<RequestOtpPage/>}/>
         <Route path='/*' element={<div>Error Page</div>}/>
+        <Route path='/google-auth-redirect' element={<LoginPage />} />
       </Routes>
     </Suspense>
   )
